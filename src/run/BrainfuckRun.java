3b061ba2434a2ab647680d427c97a504c5bf7c98
xbony2 <xbony22@gmail.com>
2014-11-19 17:48:33 -0500

    Better information files (yay HTML :P)

--- src/run/BrainfuckRun.java
+++ src/run/BrainfuckRun.java
@@ -7,7 +7,7 @@ import org.faabtech.brainfuck.BrainfuckEngine;
 
 public class BrainfuckRun {
 	
-	private static final String FILE_LOCATION = "samples/brainfuck/cat.bf";
+	private static final String FILE_LOCATION = "samples/brainfuck/self_interpreter.bf";
 	
 	/**
 	 * To change the file location or name, just change the string FILE_lOCATION in this class.
